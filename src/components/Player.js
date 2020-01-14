0ed7233d01fdb5b663f7229ce70adc872373a14d
Takamichi Omori <merrytakamerry@gmail.com>
2020-01-14 20:44:45 +0900

    API呼び出しと音声再生

---
+++ src/components/Player.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
+
+
+
+export default class Player extends React.Component {
+    constructor(props){
+        super(props);
+        this.pauseAudio = this.pauseAudio.bind(this);
+        this.playAudio = this.playAudio.bind(this)
+        this.state = {
+            isPlaying : false,
+            song: '',
+            audio: new Audio(this.props.audioURL),
+        }
+    }
+    pauseAudio() {
+        console.log("pause")
+        this.state.audio.pause()
+
+        // 最初から再生する場合
+        let newAudioState = this.state.audio;
+        newAudioState.currentTime = 0
+        this.setState({audio:newAudioState})
+
+        this.setState({isPlaying: !this.state.isPlaying})
+    }
+    playAudio(){
+        console.log("play")
+        console.log(this)
+        this.state.audio.play()
+        this.setState({isPlaying: !this.state.isPlaying})
+    }
+    render() {
+        return (
+            this.state.isPlaying?
+                <StopIcon onClick={this.pauseAudio}>
+                </StopIcon> 
+                : 
+                <PlayArrowIcon onClick={this.playAudio}/>
+        )
+    }
+}
