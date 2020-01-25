d3fb314b11a54f6ab4d415f2722c104072ef3485
Masahisa Honda <masahisa0910@gmail.com>
2020-01-25 22:43:16 +0900

    add sample redux code

--- src/containers/BottakuriVoiceContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import App from '../components/App'
-
-function mapStateToProps(state) {
-  return state
-}
-
-export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import reducer from './reducers/index';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom";
 
-const store = createStore(reducer);
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.render( <Provider store={store}><App /></Provider>, document.getElementById('root'));
+import TodoApp from "./TodoApp";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  rootElement
+);
